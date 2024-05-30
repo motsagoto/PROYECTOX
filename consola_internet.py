5e79a4ea3f5e39c598f27a037b791eb42df06069
motsagoto <tomas.sandoval@urosario.edu.co>
2024-05-29 21:17:49 -0500

    Add files via upload

--- consola_internet.py
+++ consola_internet.py
@@ -25,81 +25,86 @@ def ejecutar_cargar_datos() -> pd.DataFrame:
         print(datos)
     return datos
 
-def ejecutar_piechart_anio(datos:pd.DataFrame, anio:int )->None:
+def ejecutar_piechart_anio(datos: pd.DataFrame, anio: int) -> None:
     """Solicita al usuario que ingrese un anio y muestra un piechart con la distribucion de accesos fijos a internet por departamento.
     Parametros:
         datos (pd.DataFrame): El dataframe con los datos del acceso a internet en Colombia.
+        anio (int): El año para el cual se quiere generar el pie chart.
     """
-    anio=int(input("Ingrese un año: "))
-    print(mod.piechart_anio(datos, anio))
-    
-def ejecutar_diagrama_barras(datos: pd.DataFrame, Departamento: str)->None:
+    if datos is not None and not datos.empty:
+        mod.piechart_anio(datos, anio)
+    else:
+        print("No se han cargado datos. Por favor cargue los datos primero.")
+
+def ejecutar_diagrama_barras(datos: pd.DataFrame, departamento: str) -> None:
     """Muestra un diagrama de barras con la relacion entre el numero de accesos fijos a internet y la poblacion por municipio.
     Parametros:
         datos (pd.DataFrame): El dataframe con los datos del acceso a internet en Colombia.
+        departamento (str): El departamento para el cual se quiere generar el diagrama de barras.
     """
-    Departamento=input("Ingrese un departamento: ")
-    print(mod.diagrama_barras(datos,Departamento))
-   
-   
-   
-def ejecutar_diagrama_cajas(datos: pd.DataFrame,Departamento: str):
+    if datos is not None and not datos.empty:
+        mod.diagrama_barras(datos, departamento)
+    else:
+        print("No se han cargado datos. Por favor cargue los datos primero.")
+
+def ejecutar_diagrama_cajas(datos: pd.DataFrame, departamento: str) -> None:
     """Muestra un diagrama de cajas con la distribucion de accesos fijos a internet por provincia en un departamento.
     Parametros:
         datos (pd.DataFrame): El dataframe con los datos del acceso a internet en Colombia.
+        departamento (str): El departamento para el cual se quiere generar el diagrama de cajas.
     """
-    Departamento=input("Ingrese un departamento: ")
-    print(mod.diagrama_cajas(datos, Departamento))
-    
-def crear_matriz(datos: pd.DataFrame)->tuple:
+    if datos is not None and not datos.empty:
+        mod.diagrama_cajas(datos, departamento)
+    else:
+        print("No se han cargado datos. Por favor cargue los datos primero.")
+
+def crear_matriz(datos: pd.DataFrame) -> tuple:
     """Crea una matriz con la cantidad de accesos fijos a internet por departamento y anio.
     Parametros:
         datos (pd.DataFrame): El dataframe con los datos del acceso a internet en Colombia.
     Retorno: tuple
         La matriz con la cantidad de accesos fijos a internet por departamento y anio.
     """
-    matriz = mod.crear_matriz(datos)
-    print("Se ha creado la matriz con la cantidad de accesos fijos a internet por departamento y anio.")
-    print (matriz)
-    return matriz
+    if datos is not None and not datos.empty:
+        matriz = mod.crear_matriz(datos)
+        print("Se ha creado la matriz con la cantidad de accesos fijos a internet por departamento y anio.")
+        return matriz
+    else:
+        print("No se han cargado datos. Por favor cargue los datos primero.")
+        return ([], {}, {})
 
-    
-def ejecutar_cantidad_accesos_anio(matriz: tuple,anio:int, datos: pd.DataFrame)->int:
+def ejecutar_cantidad_accesos_anio(matriz: tuple, anio: int, datos: pd.DataFrame) -> None:
     """Muestra la cantidad de accesos fijos a internet en un anio.
     Parametros:
         matriz (tuple): La matriz con la cantidad de accesos fijos a internet por departamento y anio.
+        anio (int): El año para el cual se quiere consultar la cantidad de accesos.
+        datos (pd.DataFrame): El dataframe con los datos del acceso a internet en Colombia.
     """
-    anio=int(input("Porfavor ingrese un año: "))
-    print("La cantidad de accesos fijos a internet en el año " + str(anio) + " es de " + str(mod.cantidad_accesos_anio(matriz,anio,datos)))
-   
-def ejecutar_departamento_en_ascenso(matriz: tuple,anio:int, porcentaje: float,  datos: pd.DataFrame)->tuple:
+    if matriz and anio in matriz[1].values():
+        cantidad = mod.cantidad_accesos_anio(matriz, anio, datos)
+        print(f"La cantidad de accesos fijos a internet en el año {anio} es de {cantidad}.")
+    else:
+        print("No se ha creado la matriz o el año no es válido. Por favor cargue los datos y cree la matriz primero.")
+
+def ejecutar_departamento_en_ascenso(matriz: tuple, anio: int, porcentaje: float, datos: pd.DataFrame) -> None:
     """Muestra el primer departamento que supera el porcentaje de incremento de accesos fijos a internet en un anio dado.
     Parametros:
         matriz (tuple): La matriz con la cantidad de accesos fijos a internet por departamento y anio.
+        anio (int): El año para el cual se quiere verificar el ascenso.
+        porcentaje (float): El porcentaje de incremento a verificar.
+        datos (pd.DataFrame): El dataframe con los datos del acceso a internet en Colombia.
     """
-    anio=input("Porfavor ingrese un año: ")
-    porcentaje=input("Porfavor ingrese un porcentaje por el cual desee comparar: ")
-    llamar=print(mod.departamento_en_ascenso(matriz, anio, porcentaje, datos))
-    #if llamar(departamento_mayor_crecimiento)!=("Ninguno",-101) :
-        #respuesta=print(llamar(departamento_mayor_crecimiento) +" tuvo un crecimiento del " + llamar(round(porcentaje_superior),2) + "% del año " + str(anio) + " al año " + str(anio+1))
-   # else:
-       # respuesta=print("No se encontró ningún departamento que supere el porcentaje especificado.")
-
-   
-           
-    
-def ejecutar_departamentos_mapa(matriz: tuple):
-    """Genera un mapa con la cantidad de accesos fijos a internet por departamento en un anio.
-    Parametros:
-        matriz (tuple): La matriz con la cantidad de accesos fijos a internet por departamento y anio.
-    """
-    #Complete la siguiente función solamente si desea implementar el bono 
-    #TODO Completar?
-
+    if matriz and anio in matriz[1].values():
+        resultado = mod.departamento_en_ascenso(matriz, anio, porcentaje, datos)
+        if resultado != ("Ninguno", -101):
+            print(f"{resultado[0]} tuvo un crecimiento del {round(resultado[1], 2)}% del año {anio} al año {anio+1}.")
+        else:
+            print("No se encontró ningún departamento que supere el porcentaje especificado.")
+    else:
+        print("No se ha creado la matriz o el año no es válido. Por favor cargue los datos y cree la matriz primero.")
 
 def mostrar_menu():
-    """Imprime las opciones de ejecucion disponibles para el usuario.
-    """
+    """Imprime las opciones de ejecucion disponibles para el usuario."""
     print("\nOpciones")
     print("1. Cargar datos sobre el acceso a internet en Colombia.") 
     print("2. Mostrar Top 20 departamentos con mayor numero de accesos fijos a internet en un anio.") 
@@ -112,32 +117,34 @@ def mostrar_menu():
     #print("8. Generar mapa de la cantidad de accesos fijos a internet por departamento en un anio.")
     print("9. Salir.")
 
-
 def iniciar_aplicacion():
     """Ejecuta el programa para el usuario."""
     continuar = True
-    datos = ""
-    anio=0
-    Departamento=""
-    porcentaje=0
-    matriz = []
+    datos = pd.DataFrame()
+    matriz = ()
     while continuar:
         mostrar_menu()
         opcion_seleccionada = int(input("Por favor seleccione una opcion: "))
         if opcion_seleccionada == 1:
             datos = ejecutar_cargar_datos()
         elif opcion_seleccionada == 2:
-            ejecutar_piechart_anio(datos,anio)
+            anio = int(input("Ingrese un año: "))
+            ejecutar_piechart_anio(datos, anio)
         elif opcion_seleccionada == 3:
-            ejecutar_diagrama_barras(datos,Departamento)
+            departamento = input("Ingrese un departamento: ")
+            ejecutar_diagrama_barras(datos, departamento)
         elif opcion_seleccionada == 4:
-            ejecutar_diagrama_cajas(datos, Departamento)
+            departamento = input("Ingrese un departamento: ")
+            ejecutar_diagrama_cajas(datos, departamento)
         elif opcion_seleccionada == 5:
             matriz = crear_matriz(datos)
         elif opcion_seleccionada == 6:
-            ejecutar_cantidad_accesos_anio(matriz, anio , datos)
+            anio = int(input("Por favor ingrese un año: "))
+            ejecutar_cantidad_accesos_anio(matriz, anio, datos)
         elif opcion_seleccionada == 7:
-            ejecutar_departamento_en_ascenso(matriz,anio, porcentaje)
+            anio = int(input("Por favor ingrese un año: "))
+            porcentaje = float(input("Por favor ingrese un porcentaje por el cual desee comparar: "))
+            ejecutar_departamento_en_ascenso(matriz, anio, porcentaje, datos)
         #Descomente las siguientes dos líneas si desea implementar el bono. 
         #elif opcion_seleccionada == 8:
             #ejecutar_departamentos_mapa(matriz)
@@ -146,5 +153,5 @@ def iniciar_aplicacion():
         else:
             print("Por favor seleccione una opcion valida.")
 
-#PROGRAMA PRINCIPAL
+# PROGRAMA PRINCIPAL
 iniciar_aplicacion()
--- internet.py
+++ internet.py
@@ -5,225 +5,102 @@ Modulo de funciones.
 
 @author: Cupi2
 """
-#llmas las librerias que necesitas.
 import pandas as pd
 import matplotlib.pyplot as plot
 
-#Parte 1(listo)
-#Cargar datos del archivo.csv
-def cargar_datos(archivo:str)->pd.DataFrame:
-    llamar=pd.read_csv(archivo,sep=",")
+#Parte 1 (listo)
+def cargar_datos(archivo: str) -> pd.DataFrame:
+    llamar = pd.read_csv(archivo, sep=",")
     return llamar
 datos = cargar_datos("internet.csv")
-#print(datos)
 
-#Parte 2
-#Requerimiento 1 (listo)
-#Crear una grafica de torta de los departamentos y sus accesos de internet en un año.
-def piechart_anio(datos: pd.DataFrame, anio:int)->None: 
-    #creo un copia del DataFrame para filtrar solo los datos que necesito.
-    copia=datos[["AÑO","DEPARTAMENTO", "No. ACCESOS FIJOS"]].copy()
-    #Comparar el año dentro de la copia con al año que ingresa por parametro.
-    datos_anio = datos[copia["AÑO"] == anio]
-    # Agrupar por departamento y contar los accesos
+# Parte 2
+# Requerimiento 1 (listo)
+def piechart_anio(datos: pd.DataFrame, anio: int) -> None:
+    copia = datos[["AÑO", "DEPARTAMENTO", "No. ACCESOS FIJOS"]].copy()
+    datos_anio = copia[copia["AÑO"] == anio]
     accesos_por_departamento = datos_anio.groupby("DEPARTAMENTO")["No. ACCESOS FIJOS"].sum()
-    # Obtener el top 20 departamentos
     top_20_departamentos = accesos_por_departamento.nlargest(20)
-    top_20_departamentos.plot(kind="pie",autopct="%0.1f%%",figsize=(10,8), title="Top 20 Departamentos con Accesos Fijos a Internet en "+ str(anio))
-    # Mostrar el plot
-    plot.show()    
-#piechart_anio(datos, 2021)
+    top_20_departamentos.plot(kind="pie", autopct="%0.1f%%", figsize=(10, 8), title="Top 20 Departamentos con Accesos Fijos a Internet en " + str(anio))
+    plot.show()
  
-#Requerimiento 2(listo)
-def diagrama_barras(datos: pd.DataFrame, Departamento: str)->None:
-    #Crear una copia del DataFrame
-    copia=datos.copy()
-    #Se compara el Departamento que ingresa por parametro y el que esta dentro de copia del DataFrame
-    datos_filtrados = datos[copia["DEPARTAMENTO"] == Departamento]
-    #Agrupo los Municipios.
-    agrupaciones=datos_filtrados.groupby("MUNICIPIO")    
+# Requerimiento 2 (listo)
+def diagrama_barras(datos: pd.DataFrame, departamento: str) -> None:
+    copia = datos.copy()
+    datos_filtrados = copia[copia["DEPARTAMENTO"] == departamento]
+    agrupaciones = datos_filtrados.groupby("MUNICIPIO")    
     suma1 = agrupaciones["No. ACCESOS FIJOS"].sum()
     suma2 = agrupaciones["POBLACIÓN DANE"].sum()
-    #realizaos una división de las dos sumas que se realizaro anteriormente.
-    division = (suma1/suma2).sort_values(ascending=False)
-    # Obtener el top 20 departamentos
+    division = (suma1 / suma2).sort_values(ascending=False)
     top_20_departamentos = division.head(20)
-    top_20_departamentos.plot(kind="bar", figsize=(13,7), fontsize="small", ylim=(0.0,0.40), title="Top 20 municipios con mayor relación de Accesos Fijos a internet por población en " + Departamento , ylabel=("Relación accesos fijos a internet por población"), xlabel=" Municipios de" + Departamento)
-    
-    # Mostrar el plot
+    top_20_departamentos.plot(kind="bar", figsize=(13, 7), fontsize="small", ylim=(0.0, 0.40), title="Top 20 municipios con mayor relación de Accesos Fijos a internet por población en " + departamento, ylabel="Relación accesos fijos a internet por población", xlabel="Municipios de " + departamento)
     plot.show()
-#diagrama_barras(datos, "Santander") 
-   
-#Requerimiento 3(listo)
-def diagrama_cajas(datos: pd.DataFrame, Departamento: str) -> None:
-    """Genera un diagrama de caja y bigotes que muestra la distribución de accesos fijos a internet por provincia en un departamento específico.
-    
-    Parámetros:
-        datos (pd.DataFrame): El dataframe con los datos del acceso a internet en Colombia.
-        departamento (str): El nombre del departamento para el cual se generará el gráfico.
-    """
-    #Se hizo un copia del DataFrame que ingresa como parametro.
-    copia=datos[["PROVINCIA","DEPARTAMENTO", "No. ACCESOS FIJOS"]].copy()
-    # Filtrar los datos para el departamento especificado
-    datos_filtrados = datos[copia["DEPARTAMENTO"] == Departamento]
-    segundo_filtro = datos_filtrados[["PROVINCIA","No. ACCESOS FIJOS"]]
-    provincia=segundo_filtro.groupby("PROVINCIA")
-    
-    # Crear el diagrama de caja y bigotes
-    provincia.boxplot(figsize=(14, 8),vert=False, by="PROVINCIA", showfliers=False,sharey=False)
 
-    # Ajustar el título y etiquetas
-    plot.title("Diagrama de caja de accesos fijos por provincias en el departamento de "+ Departamento)
-    plot.suptitle("")  # Eliminar el título por defecto generado por `pandas`
+# Requerimiento 3 (listo)
+def diagrama_cajas(datos: pd.DataFrame, departamento: str) -> None:
+    copia = datos[["PROVINCIA", "DEPARTAMENTO", "No. ACCESOS FIJOS"]].copy()
+    datos_filtrados = copia[copia["DEPARTAMENTO"] == departamento]
+    segundo_filtro = datos_filtrados[["PROVINCIA", "No. ACCESOS FIJOS"]]
+    provincia = segundo_filtro.groupby("PROVINCIA")
+    provincia.boxplot(figsize=(14, 8), vert=False, by="PROVINCIA", showfliers=False, sharey=False)
+    plot.title("Diagrama de caja de accesos fijos por provincias en el departamento de " + departamento)
+    plot.suptitle("")
     plot.ylabel("No. ACCESOS FIJOS")
     plot.xlabel("No. de accesos fijos a internet") 
-    plot.xticks(rotation=45, fontsize='small')  # Ajustar la rotación y el tamaño de la fuente de las etiquetas del eje x
-    
-    # Ajustar el espacio entre subplots
+    plot.xticks(rotation=45, fontsize='small')
     plot.subplots_adjust(hspace=0.4, left=0.2)
-    
-    # Mostrar el plot
     plot.show()
 
-#diagrama_cajas(datos, "Cauca")
-    
 # Parte 3
-#Requerimiento 4(listo)
-def crear_matriz(datos: pd.DataFrame)->tuple:
-    #Esqueleto diccionarios
-    deptos =  sorted(datos["DEPARTAMENTO"].unique())
+# Requerimiento 4 (listo)
+def crear_matriz(datos: pd.DataFrame) -> tuple:
+    deptos = sorted(datos["DEPARTAMENTO"].unique())
     dept_dict = dict(list(enumerate(deptos)))
-    anios =  sorted(datos["AÑO"].unique())
+    anios = sorted(datos["AÑO"].unique())
     anios_dict = dict(list(enumerate(anios)))
-    #TODO completar la construcción de la matriz
-    matriz_final=[]
+    matriz_final = []
     for i in dept_dict:
-        fila_vacia=[]
+        fila_vacia = []
         depto = dept_dict[i]
-        filtro_depto= datos[datos["DEPARTAMENTO"]==depto] 
+        filtro_depto = datos[datos["DEPARTAMENTO"] == depto] 
         for j in anios_dict:
-            anio=anios_dict[j]
-            #Comparar el año dentro del primer fltro con el año que ingresa por parametro.
-            filtro_anio=filtro_depto[filtro_depto["AÑO"]==anio] 
+            anio = anios_dict[j]
+            filtro_anio = filtro_depto[filtro_depto["AÑO"] == anio] 
             fila_vacia.append(filtro_anio["No. ACCESOS FIJOS"].sum())
         matriz_final.append(fila_vacia)
-
     return (matriz_final, anios_dict, dept_dict)
 
-#Requerimiento 5
-def cantidad_accesos_anio(matriz: tuple, anio:int, datos: pd.DataFrame)-> int:
-   # n debe retornar el valor que representa la cantidad de accesos fijos totales en el año dado por el usuario
-   matriz_1=crear_matriz(cargar_datos("Internet.csv"))
-   suma=0
-   casilla=matriz_1[1]
-   pos_fila=None
-   for i in casilla: # range(0,len(matriz[0])):
-       dato=casilla[i]
-       if dato==anio: # matriz[0][i]==anio:
-            pos_fila=i
-   matriz_2=matriz_1
-   for j in matriz_2: #range(1,len(matriz)):
-       str(suma)+=str(j[pos_fila])
-   return int(suma)
+# Requerimiento 5
+def cantidad_accesos_anio(matriz: tuple, anio: int, datos: pd.DataFrame) -> int:
+    _, anios_dict, _ = matriz
+    pos_anio = None
+    for key, value in anios_dict.items():
+        if value == anio:
+            pos_anio = key
+            break
+    if pos_anio is None:
+        return 0
+    total_accesos = sum(fila[pos_anio] for fila in matriz[0])
+    return total_accesos
 
-
-
-
-#Requerimiento 6
-def departamento_en_ascenso(matriz: tuple, anio: int, porcentaje: float, datos: pd.DataFrame)->tuple:
-   #Comparar el año dentro de la copia con al año que ingresa por parametro.
-    datos_filtrados = datos[datos["AÑO"] == anio]
-    if anio>= 2023 or anio<2015:
-        return ("Ninguno",-101)
-    tupla_1=[]
-    # orden alfabético
-    orden_dept =  sorted(datos["DEPARTAMENTO"].unique())
-    matriz_final, anios_dict, dept_dict=matriz 
-    #compara el año qu epaa por parametro con el que se encuentra en datos(el DataFrame)
-    # Excluir el último año
-    anio_s= anio+1
-    accesos_anio_s= "accesos_anio"+anio_s
-    departamento_con_mayor_crecimiento = ""
-    porcentaje_superado = 0
-    for i in orden_dept:
-       # revision=
-        cambio_porcentual = ("""acessos del anio_s -acessos del anio""") / 100
-        if cambio_porcentual > porcentaje:
-            departamento_mayor_crecimiento = ["DEPARTAMENTO"]
-            porcentaje_superado = cambio_porcentual
-    tupla_1.append(departamento_mayor_crecimiento, porcentaje_superado)
-    return tupla_1
-    
-    
-
-    orden_anios =  sorted(datos["AÑO"].unique())
-    
-    if anio==-1:
-        respuesta=("Ninguno",-101)
-        return respuesta
-
-
-"""
-import matplotlib.image as mpimg
-def ejecutar_departamentos_mapa(matriz: tuple):
-    def cargar_coordenadas(nombre_archivo: str) -> dict:
-        deptos = {}
-        with open(nombre_archivo, encoding="utf8") as archivo:
-            archivo.readline()  # Omitir la línea de títulos
-            for linea in archivo:
-                linea = linea.strip()
-                datos = linea.split(";")
-                deptos[datos[0]] = (int(datos[1]), int(datos[2]))
-        return deptos
-    
-    def mapa_accesos_fijos(matriz:tuple, anio:int, coordenadas:str):
-        mapa = mpimg.imread("mapa.png").tolist()
-        colores = {"0 a <5000": [0.94, 0.10, 0.10],
-                   "5000 a <10000": [0.94, 0.10, 0.85],
-                   "10000 a <20000": [0.10, 0.50, 0.94],
-                   "20000 a <500000": [0.34, 0.94, 0.10],
-                   ">=500000": [0.99, 0.82, 0.09]}
-    
-        # Obtener el diccionario de columnas
-        dict_columnas = matriz[1]
-        col_anio = -1
-        for i in range(len(dict_columnas)):
-            if dict_columnas[i] == anio:
-                col_anio = i
-
-    if col_anio == -1:
-        print("No se encontraron datos para el año" + str(anio))
-        return
-    
- #Poner colores segun el rango
-        for depto, coord in coordenadas.items():
-            accesos = matriz[0][matriz[2][depto]][col_anio]
-            for i in range(13):
-                for j in range(13):
-                    x = coord[0] - 6 + j
-                    y = coord[1] - 6 + i
-                    if 0 <= accesos < 5000:
-                        mapa[y][x] = colores["0 a <5000"]
-                    elif 5000 <= accesos < 10000:
-                        mapa[y][x] = colores["5000 a <10000"]
-                    elif 10000 <= accesos < 20000:
-                        mapa[y][x] = colores["10000 a <20000"]
-                    elif 20000 <= accesos < 500000:
-                        mapa[y][x] = colores["20000 a <500000"]
-                    else:
-                        mapa[y][x] = colores[">=500000"]
-    
-        plt.imshow(mapa)
-        legends = []
-        for rango in colores:
-            legends.append(mpatches.Patch(color=colores[rango], label=rango))
-        plt.legend(handles=legends, loc=3, fontsize='x-small')
-        plt.title(f"Accesos fijos a internet por departamento en el año {anio}", fontsize='x-small')
-        plt.show()
-    
-    # Ejemplo de uso
-    matriz = (... , ... , ...)  # Tupla con la matriz y los diccionarios
-    anio = 2020
-    coordenadas = cargar_coordenadas("coordenadas.txt")
-    mapa_accesos_fijos(matriz, anio, coordenadas)
-    """
+# Requerimiento 6
+def departamento_en_ascenso(matriz: tuple, anio: int, porcentaje: float, datos: pd.DataFrame) -> tuple:
+    if anio >= 2023 or anio < 2015:
+        return ("Ninguno", -101)
+    matriz_final, anios_dict, dept_dict = matriz
+    pos_anio = None
+    for key, value in anios_dict.items():
+        if value == anio:
+            pos_anio = key
+            break
+    if pos_anio is None or pos_anio + 1 not in anios_dict:
+        return ("Ninguno", -101)
+    max_cambio = ("Ninguno", -101)
+    for i in range(len(dept_dict)):
+        if matriz_final[i][pos_anio] == 0:
+            continue
+        crecimiento = (matriz_final[i][pos_anio + 1] - matriz_final[i][pos_anio]) / matriz_final[i][pos_anio] * 100
+        if crecimiento > porcentaje:
+            max_cambio = (dept_dict[i], crecimiento)
+            break
+    return max_cambio
